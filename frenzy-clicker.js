8c53522c263822bf5d6a617be9c97c17dd8680db
Modus Operandi <modo@modo.lv>
2019-04-22 14:11:55 +0300

    Added version check and debugging.

--- frenzy-clicker.js
+++ frenzy-clicker.js
@@ -1,26 +1,53 @@
 javascript:(function() {
-	window.modo_cc = window.modo_cc || {active: null};
+	let FC = window.FrenzyClicker || (window.FrenzyClicker = {
+		CCVersion: 2.019,
+		Version: 2.019.1
+		Checking: null,
+		Clicking: null,
+		Debug: false
+	});
 	
+	// Reset in case of reload
+	clearInterval(FC.Clicking);
+	clearInterval(FC.Checking);
+
+	// Check for version match
+	let load = true;
+	if (Game.version != FC.Version) {
+		load = confirm("Frency Clicker " + FC.Version + " is meant for Cookie Clicker version " + FC.CCVersion + ", you are playing version " + Game.version + ". Load anyway?");
+	}
+	if (!load)
+		return;
+		
+	// Set up the clicking
 	setInterval(function() {
+		if (FC.Clicking) {
+			debug("FC already clicking, nothing to do.")
+			return;
+		}
+			
+		debug("Checking for buffs.");
 		if (Game.hasBuff('Click frenzy') || Game.hasBuff('Cookie storm')) {
-			window.modo_cc.active = setInterval(function() {
-				if (Game.hasBuff('Cookie storm'))
-				{
-					Game.shimmers.forEach(function(shimmer)
-					{
+			debug("Buff active, starting clicking.");
+			FC.Clicking = setInterval(function() {
+				if (Game.hasBuff('Cookie storm')) {
+					Game.shimmers.forEach(function(shimmer) {
 					    shimmer.pop()
 					})
 				}
-				else if (Game.hasBuff('Click frenzy'))
-				{
+				else if (Game.hasBuff('Click frenzy')) {
 					Game.ClickCookie();
 				}
 				else {
-					clearInterval(window.modo_cc.active);
+					debug("Buff over, stoping clicking.");
+					clearInterval(FC.Clicking);
 				}
 			}, 100);
 		}
 	}, 500);
 	
-	
+	function debug(...params) {
+		if (FC.Debug)
+			console.debug(console, params);
+	}
 }());
