7af7d216ccc59fbd69f1b492042facfcbf933159
Modus Operandi <modo@modo.lv>
2019-04-26 14:00:06 +0300

    Version 2.019.4. Added support for Elder Frenzy, Dragonflight and Cursed Finger. Fixed Cookie Storm leftovers.

--- frenzy-clicker.js
+++ frenzy-clicker.js
@@ -1,62 +1,109 @@
-javascript:(function() {
-	let FC = window.FrenzyClicker || (window.FrenzyClicker = {
-		CCVersion: 2.019,
-		Version: "2.019.3",
-		Checking: null,
-		Clicking: null,
-		Debug: false,
-		Speed: 100,
-	});
-	
-	// Reset in case of reload
-	clearInterval(FC.Clicking);
-	clearInterval(FC.Checking);
+'use strict';
 
-	// Check for version match
-	let load = true;
-	if (Game.version != FC.CCVersion) {
-		load = confirm("Frency Clicker " + FC.Version + " is meant for Cookie Clicker version " + FC.CCVersion + ", you are playing version " + Game.version + ". Load anyway?");
-	}
-	if (!load)
-		return;
-		
-	// Set up the clicking
-	setInterval(function() {
-		if (FC.Clicking > 0) {
-			debug("Clicking big cookie (frenzy) or all golden cookies (storm) " + (1000 / FC.Speed).toFixed(2) + " times per second.")
-			return;
-		}
+javascript:(function() {
+	class FrenzyClicker {
+	
+		constructor() {
+			this.mainTicker = 0;
+			this.settings = {
+				frequency: 100,
+				debug: true,
+			};
+			this.ccVersion = 2.019,
+			this.version = "2.019.4",
+			this.clickBuffs = [
+				"Elder frenzy",
+				"Click frenzy",
+				"Cursed finger",
+				"Cookie storm",
+				"Dragonflight"
+			]
+			this.hadBuffs = false;
+			this.hadCookieStorm = false;
 			
-		if (Game.hasBuff('Click frenzy') || Game.hasBuff('Cookie storm')) {
-			debug("Buff active, starting clicking.");
-			FC.Clicking = setInterval(function() {
-				if (Game.hasBuff('Cookie storm')) {
-					Game.shimmers.forEach(function(shimmer) {
-					    shimmer.pop()
-					})
-				}
-				else if (Game.hasBuff('Click frenzy')) {
-					Game.ClickCookie();
-				}
-				else {
-					debug("Buff over, stopping clicking.");
-					FC.Clicking = clearInterval(FC.Clicking);
-				}
-			}, FC.Speed);
+			window.FrenzyClicker = this;
 		}
-		else
-			debug("Waiting for frenzy or storm.");
-	}, 500);
+		
+		
 	
-	let title = "Frenzy Clicker " + FC.Version + " loaded";
-	let text = "The big cookie (during Click Frenzy) and all golden cookies (during Cookie Storm) will be automatically clicked " 
-	+ (1000 / FC.Speed).toFixed(2) + " times per second.";
+		tick() {
+			let self = window.FrenzyClicker;
+			if (!self.hasAnyClickBuffs()) {
+				if (self.hadBuffs) {
+					self.hadBuffs = false;
+					self.debug("Buff(s) off.");
+					if (self.hadCookieStorm) {
+						self.hadCookieStorm = false;
+						self.debug("Ensuring Cookie Storm leftovers are cleaned up.");
+						while (Game.shimmers.length > 0)
+							Game.shimmers.forEach((shimmer) => shimmer.pop());
+					}
+				}
+				return;
+			}
+				
+			if (!self.hadBuffs) {
+				self.debug("Buff(s) on.");
+			}
+				
+			if (Game.hasBuff('Cookie storm')) {
+				if (!self.hadCookieStorm) {
+					self.hadCookieStorm = true;
+				}
+				Game.shimmers.forEach((shimmer) => shimmer.pop())
+			}
+			if (Game.hasBuff('Click frenzy') || Game.hasBuff('Dragonflight') || Game.hasBuff('Elder frenzy') || Game.hasBuff('Cursed finger')) {
+				Game.ClickCookie();
+			}
+			
+			self.hadBuffs = true;
+		}
+		
+		
+		hasAnyClickBuffs() {
+			return this.clickBuffs.some((buff) => Game.hasBuff(buff));
+		}
+		
+		
+		run() {
+			if (!this.isRunning()) {
+				// Check for version match
+				let load = true;
+				if (Game.version != this.ccVersion) {
+					load = confirm("Frency Clicker " + this.version + " is meant for Cookie Clicker version " + this.ccVersion + ", you are playing version " + Game.version + ". Start anyway?");
+				}
+				if (!load)
+					return false;
+
+				this.mainTicker = setInterval(this.tick, this.Speed);
+				
+				let title = "Frenzy Clicker " + this.version + " loaded";
+				let text = "Click speed: once every " + this.Speed + " milliseconds, or " + (1000 / this.Speed).toFixed(2) + " times per second.";
+				
+				Game.Notify(title, text);
+				console.info(title + ". " + text);
+			}
+			return this;
+		}
+		
+		
+		isRunning() {
+			return this.mainTicker > 0;
+		}
+		
+		
+		debug(...params) {
+			if (this.Debug)
+				console.debug.apply(console, params);
+		}
 	
-	Game.Notify(title, text);
-	console.info(title + ". " + text);
 	
-	function debug(...params) {
-		if (FC.Debug)
-			console.debug.apply(console, params);
+		get Speed() { return this.settings.frequency; }
+		set Speed(val) { return this.settings.frequency = val; }
+		
+		get Debug() { return this.settings.debug; }
+		set Debug(val) { return this.settings.debug = val; }
 	}
+	
+	return new FrenzyClicker().run();
 }());
